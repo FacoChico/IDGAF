60dd3051f98993a4692e793a5a1b31a4c0477e30
FacoChico <popov098097grigory@gmail.com>
2022-09-08 14:36:48 +0300

    Добавил "и не только"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("ЗДАРОВА, СЛАВЯНЕ (и не только)");
-        System.out.println("sheesh ");
+        System.out.println("sheesh");
+        System.out.println("Is it working?");
     }
 }
